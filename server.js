5644062eae1c0f568e9c4123a008c058b33de690
manish04042003 <manishkumar817835@gmail.com>
2023-04-15 23:14:40 +0530

    Price ID genrate

--- server.js
+++ server.js
@@ -1,18 +1,20 @@
+require("dotenv").config();
 // This is your test secret API key.
-const stripe = require('stripe')('sk_test_51Mx8HRSJ4QSCX8qS17rdfGxw9ORpjMnYlUiO4T1bdDfhOopVzKREjIKsZRYgy9C1D5dZfWr3taJGT7YOk4fEKbtl00tgXoWSfs');
+const stripe = require('stripe')(process.env.KEY);
 
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
 
 const YOUR_DOMAIN = 'http://localhost:4242';
+let priceID= ''
 
 app.post('/create-checkout-session', async (req, res) => {
   const session = await stripe.checkout.sessions.create({
     line_items: [
       {
         // Provide the exact Price ID (for example, pr_1234) of the product you want to sell
-        price: 'price_1MxBo8SJ4QSCX8qSfDF08HVK',
+        price: priceID,
         quantity: 1,
       },
     ],
@@ -25,12 +27,29 @@ app.post('/create-checkout-session', async (req, res) => {
 });
 
 
-app.post('/create-product', async (req, res) => {
+// app.post('/create-product', async (req, res) => {
+  
+// })
+async function createProduct(name){
   const product = await stripe.products.create({
-    name: 'Gold Special',
-  });
+    name: name,
+  })
   console.log(product);
-})
+  console.log('----------------------------------------------------------------------------------------')
+  const price = await stripe.prices.create({
+    unit_amount: 1200,
+    currency: 'inr',
+    product: `${product.id}`,
+  });
+  console.log(price)
+
+  priceID = price.id;
+}
+
+createProduct('manish1');
+
+
+
 
 
 app.listen(4242, () => console.log('Running on port 4242'));
